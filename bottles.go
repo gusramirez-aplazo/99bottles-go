cfcc85dc0865ae74160a7fdad1050a95a38e603e
Gus Ramírez <gustavo@aplazo.mx>
2023-01-23 16:33:05 -0600

    test: verse 0, 1, 2 and default

--- bottles.go
+++ bottles.go
@@ -7,7 +7,37 @@ import (
 func Verse(index uint) (string, error) {
 	current := index
 	previous := index - 1
-	return fmt.Sprintf(
-			"%v bottles of beer on the wall, %v bottles of beer.\nTake one down and pass it around, %v bottles of beer on the wall.\n", current, current, previous),
-		nil
+
+	switch index {
+
+	case 0:
+		return "No more bottles of beer on the wall, " +
+				"no more bottles of beer.\n" +
+				"Go to the store and buy some more, " +
+				"99 bottles of beer on the wall.\n",
+			nil
+	case 1:
+		return "1 bottle of beer on the wall, " +
+				"1 bottle of beer.\n" +
+				"Take it down and pass it around, " +
+				"no more bottles of beer on the wall.\n",
+			nil
+	case 2:
+		return "2 bottles of beer on the wall, " +
+				"2 bottles of beer.\n" +
+				"Take one down and pass it around, " +
+				"1 bottle of beer on the wall.\n",
+			nil
+	default:
+		return fmt.Sprintf(
+				"%v bottles of beer on the wall, ", current) +
+				fmt.Sprintf(
+					"%v bottles of beer.\n", current) +
+				"Take one down and pass it around, " +
+				fmt.Sprintf(
+					"%v bottles of beer on the wall.\n", previous),
+			nil
+
+	}
+
 }
--- bottles_test.go
+++ bottles_test.go
@@ -50,3 +50,72 @@ func TestAnotherVerse(t *testing.T) {
 			verse)
 	}
 }
+
+func TestVerse2(t *testing.T) {
+	const verseNum uint = 2
+
+	expected := "2 bottles of beer on the wall, " +
+		"2 bottles of beer.\n" +
+		"Take one down and pass it around, " +
+		"1 bottle of beer on the wall.\n"
+
+	verse, err := Verse(verseNum)
+	if verse == "" || err != nil {
+		t.Errorf(
+			"Can't retrieve verse num: %v",
+			verseNum)
+	}
+
+	if verse != expected {
+		t.Errorf(
+			"Incorrect output>> \nExpected:\n%v\nActual:\n%v\n",
+			expected,
+			verse)
+	}
+}
+
+func TestVerse1(t *testing.T) {
+	const verseNum uint = 1
+
+	expected := "1 bottle of beer on the wall, " +
+		"1 bottle of beer.\n" +
+		"Take it down and pass it around, " +
+		"no more bottles of beer on the wall.\n"
+
+	verse, err := Verse(verseNum)
+	if verse == "" || err != nil {
+		t.Errorf(
+			"Can't retrieve verse num: %v",
+			verseNum)
+	}
+
+	if verse != expected {
+		t.Errorf(
+			"Incorrect output>> \nExpected:\n%v\nActual:\n%v\n",
+			expected,
+			verse)
+	}
+}
+
+func TestVerse0(t *testing.T) {
+	const verseNum uint = 0
+
+	expected := "No more bottles of beer on the wall, " +
+		"no more bottles of beer.\n" +
+		"Go to the store and buy some more, " +
+		"99 bottles of beer on the wall.\n"
+
+	verse, err := Verse(verseNum)
+	if verse == "" || err != nil {
+		t.Errorf(
+			"Can't retrieve verse num: %v",
+			verseNum)
+	}
+
+	if verse != expected {
+		t.Errorf(
+			"Incorrect output>> \nExpected:\n%v\nActual:\n%v\n",
+			expected,
+			verse)
+	}
+}
